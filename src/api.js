387620d61766aa68186165ecff53e6d7225a193b
Brian Purnell <purnellbp@gmail.com>
2020-03-13 14:00:27 -0500

    add movie funciton

--- src/api.js
+++ src/api.js
@@ -1,6 +1,40 @@
+
 module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
+  },
+  postMovie: (newMovie) => {
+    return fetch('api/movies', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newMovie)
+    })
+        .then(response => response.json());
+  },
+  editMovie: (id, book) => {
+    return fetch(`api/movies/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json.stringify(book)
+    })
+        .then(response => resopnse.json());
+  },
+  deleteMovie: (id) => {// a delete is final so we want a confirm
+    return fetch(`api/movies/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+        .then(response => response.json());
   }
 };
+
+
+// OMDB API Call variables
+
--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-/**
- * es6 modules and imports
- */
-import sayHello from './hello';
-sayHello('World');
+var $ = require("jquery");
 
-/**
- * require style imports
- */
 const {getMovies} = require('./api.js');
+const {postMovie} = require('./api.js');
+const {editMovie} = require('./api.js');
+const {deleteMovie} = require('./api.js');
+
+
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+    // console.log(`id#${id} - ${title} - rating: ${rating}`);
+    $("#movieList").append(` </br>  id#${id} - ${title} - rating: ${rating}   `);
+
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+// Post new movie
+
+
+const newMovie = {title: 'Animal House', rating: '9'};
+
+postMovie(newMovie);
+
+
+
+
