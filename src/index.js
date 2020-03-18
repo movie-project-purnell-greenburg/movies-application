7b6855d121f5102974aa3d2cb2f92e66be2e784b
Brian Purnell <purnellbp@gmail.com>
2020-03-17 21:56:31 -0500

    added admin modal, delete button

--- src/index.js
+++ src/index.js
@@ -1,119 +1,108 @@
 var $ = require("jquery");
 require("jquery-modal");
 
+
 const {getMovies} = require('./api.js');
 const {postMovie} = require('./api.js');
 const {editMovie} = require('./api.js');
 const {deleteMovie} = require('./api.js');
 
-$(document).ready(()=>{
-   loadGallery();
+$(document).ready(() => {
+    loadGallery();
+    adminMenu();
 });
 
 
 // Query movies from local DB and displays them
 const loadGallery = () => {
-   $('#catalogue').empty();
+    $('#catalogue').empty();
+
     getMovies()
         .then((movies) => {
-        // Loads catalogue
-        movies.forEach(({title, id, poster}) => {
-            // console.log(`id#${id} - ${title} - rating: ${rating}`);
-            $("#catalogue").append(` <div class="movie-block" id="${id}">  ${poster} <br> ${title}</div>   `);
-        });
 
-            $('.movie-block').click(function() { // grabs the local db ID of the movie clicked
-                var localID = $(this).attr("id");
-                $("#galleryView").modal({
-                    fadeDuration: 100},
-                );
-                getMovies().then((movies)=>{
-                    // console.log(movies);
-                    movies.forEach(({id, poster, plot,title,year,rated,ratings,director,actors,released,runtime})=> {
-                        if( parseInt(localID) === id ){
-                            console.log("match");
-                            $('#galleryView').append(`<h2 style="display:inline">${title}</h2>
-<div style="font-weight:lighter; display:inline; float:right;">${year} | ${rated} | ${ratings[0].Value}</div><hr>
-            <div class="parent">
-            <div class="div1"> ${poster} </div>
-            <div class="div2">  </div>
-            <div class="div3">
-            <p>${plot}</p>
-            <p> Director: ${director}</p>
-            <p> Actors: ${actors}</p>
-            <p> Release Date: ${released}</p>
-            <p> Runtime: ${runtime}</p>
-            </div>`)
-                        }
-                    })
-                });
+            // Loads catalogue
+            movies.forEach(({title, id, poster}) => {
+                // console.log(`id#${id} - ${title} - rating: ${rating}`);
+                $("#catalogue").append(` <div class="movie-block" id="${id}">  ${poster} <div class="poster-text">${title}</div> </div>   `);
             });
 
+            $('.movie-block').click(function () { // grabs the local db ID of the movie clicked
+                var localID = $(this).attr("id");
+                $("#galleryView").empty().modal({
+                        fadeDuration: 100
+                    },
+                );
+                getMovies().then((movies) => {
+                    // console.log(movies);
+                    movies.forEach(({id, poster, plot, title, year, rated, ratings, director, actors, released, runtime}) => {
+                        if (parseInt(localID) === id) {
+                            $('#galleryView').append(`<h2 class="editable">${title}</h2><div style="font-weight:lighter; display:inline; float:right;">${year} | ${rated} | ${ratings[0].Value}</div><hr>
+                                                        <div class="parent">
+                                                        <div class="div1"> ${poster} </div>
+                                                        <div class="div2">  </div>
+                                                        <div class="div3">
+                                                        <p>${plot}</p>
+                                                        <p> Director: ${director}</p>
+                                                        <p> Actors: ${actors}</p>
+                                                        <p> Release Date: ${released}</p>
+                                                        <p> Runtime: ${runtime}</p>
+                                                        </div>`)
+                        }
+                    });
+                inlineEdit();
+                });
+            });
         })
         .catch((error) => {
-        // alert('Oh no! Something went wrong.\nCheck the console for details.');
-        console.log(error);
+            // alert('Oh no! Something went wrong.\nCheck the console for details.');
+            console.log(error);
         });
 };
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 // This is the autocomplete for searching movies. Onve the movie is found in the IMDb the data is sent to our local DB
 $("#titleSearch").keyup(function () { // This is watching the text input for autocomplete
-  var keyPress = this.value;
+    var keyPress = this.value;
     console.log(keyPress);
 
     // This literally triggers an API search every keypress
-  $('#searchInput').append('<ul id="autocompleteWrapper"></ul>\n');
-    $('#autocompleteWrapper').focusout(() => { // removes search results with the input box loses focus
-        $(this).empty();
+    $('#searchInput').append('<ul id="autocompleteWrapper"></ul>\n');
+    $('#search-group').focusout(() => { // removes search results with the input box loses focus
+        $('#autocompleteWrapper').remove();
     });
-    if(keyPress === "" ){ // if the search input is empty, user hits delete or whatever, the listings are removed.
+    if (keyPress === "") { // if the search input is empty, user hits delete or whatever, the listings are removed.
         $('#autocompleteWrapper').empty();
     }
-  var url = "http://www.omdbapi.com/?apikey=1b3199ec&s=" + keyPress;
-  $.get(url)
-      .done(function (data) {
-          var results = data.Search;
-        var movieList = []; // A little bucket to hold the search results from OMDB
-        results.forEach(({Title, Poster, imdbID}) => {
-            var displayPoster = `<img class="moviePoster" style="" src="${Poster}">`; // display poster in autocomplete
-            if(Poster === "N/A"){
-                displayPoster = `<div class="noPoster"><small>N/A</small></div>`; // some movies return "N/A" for poster this handles that
-            } // now the fetch has gotten our data. Time to dump it in this bucket.
-            movieList.push(`<li> <a href="#movieDetails" rel="modal:open" class="listingItemContainer" id="${imdbID}">${displayPoster} 
+    var url = "http://www.omdbapi.com/?apikey=1b3199ec&s=" + keyPress;
+    $.get(url)
+        .done(function (data) {
+            var results = data.Search;
+            var movieList = []; // A little bucket to hold the search results from OMDB
+            results.forEach(({Title, Poster, imdbID}) => {
+                var displayPoster = `<img class="moviePoster" style="" src="${Poster}">`; // display poster in autocomplete
+                if (Poster === "N/A") {
+                    displayPoster = `<div class="noPoster"><small>N/A</small></div>`; // some movies return "N/A" for poster this handles that
+                } // now the fetch has gotten our data. Time to dump it in this bucket.
+                movieList.push(`<li> <a href="#movieDetails" rel="modal:open" class="listingItemContainer" id="${imdbID}">${displayPoster} 
                             <div class="titleTextList"> ${Title} </div> </a> </li><br> `);
-        });
-          $('#autocompleteWrapper').html(movieList); // This renders the autocomplete DIV with the results
+            });
+            $('#autocompleteWrapper').html(movieList); // This renders the autocomplete DIV with the results
 
-          $('.listingItemContainer').on('click', function () {
-              var imdbLink = $(this).attr("id"); // This grabs the internet movie databse ID so we can plug it into a new URL for a detailed search.
-              console.log(imdbLink);
-              $('#autocompleteWrapper').remove(); // removes the autocomplete div
-              // < open add movie confirm>
-              addMovieModal(imdbLink); // sends the new URL element to be added to the local database
-          })
-      })
-      .catch((onerror)=>{
-          $('#autocompleteWrapper').empty();
-      })
+            $('.listingItemContainer').on('click', function () {
+                var imdbLink = $(this).attr("id"); // This grabs the internet movie databse ID so we can plug it into a new URL for a detailed search.
+                console.log(imdbLink);
+                $('#autocompleteWrapper').remove(); // removes the autocomplete div
+                // < open add movie confirm>
+                addMovieModal(imdbLink); // sends the new URL element to be added to the local database
+            })
+        })
+        .catch((onerror) => {
+            $('#autocompleteWrapper').empty();
+        })
 });
 
 
-
 // const mainMovieModal
-
 
 
 const addMovieModal = (imdbData) => { // opens a modal with all data pulled from OMDB
@@ -123,7 +112,7 @@ const addMovieModal = (imdbData) => { // opens a modal with all data pulled from
     // TODO: display loading animation
     // pulls all omdb data and prepares it to be added locally using a bucket "newMovieBucket"
     $.get(searchLink)
-        .done((d)=>{
+        .done((d) => {
             var newMovieBucket = {
                 title: d.Title,
                 year: d.Year,
@@ -160,7 +149,7 @@ const addMovieModal = (imdbData) => { // opens a modal with all data pulled from
                 // verify movie is not in our local DB.
                 getMovies().then((movies) => {
                     var stopped = false;
-                    for(let {imdbid} of movies ) {
+                    for (let {imdbid} of movies) {
                         if (imdbid === d.imdbID) { // This checks if the new movie is already in our collection.
                             //TODO: add modal alert "This movie already here!"
                             console.log('already added');
@@ -168,33 +157,87 @@ const addMovieModal = (imdbData) => { // opens a modal with all data pulled from
                             break;
                         }
                     }
-                    if (!stopped){
+                    if (!stopped) {
                         console.log("this has not been added");
-                    postMovie(newMovieBucket); // Adds new movie to local DB.
-                    loadGallery();
+                        postMovie(newMovieBucket); // Adds new movie to local DB.
+                        loadGallery();
                     }
                 })
             });
-                // then go to page display of movie ??
+            // then go to page display of movie ??
         })
-    };     // todo: loading complete, kill loading animation here
+};     // todo: loading complete, kill loading animation here
 
 
 // admin options.. just delete function for now
-$('#admin-button').on('click', ()=>{
-    $('#admin-options').modal({
-        fadeDuration: 1000,
-        fadeDelay: 0.20
-    });
-    getMovies().then((movies) => {
-        // Loads catalogue
-        movies.forEach(({title, id, poster}) => {
-            // console.log(`id#${id} - ${title} - rating: ${rating}`);
-            $("#admin-options").append(` <div class="movie-block" id="${id}">   ${title} </div> <br>  `);
-            // $('')
-
+const adminMenu = () => {
+    $('#admin-button').on('click', () => {
+        $('#admin-options').modal({
+            fadeDuration: 1000,
+            fadeDelay: 0.20
         });
+        getMovies().then((movieList) => {
+            $("#admin-options").empty().append(`<div><h2>Administrate Justice!</h2> <hr></div>`);
+            movieList.forEach(({title, id, year}) => {
+                // console.log(`id#${id} - ${title} - rating: ${rating}`);
+                $("#admin-options").append(` <div class="movie-block" id="${id}">${title} | ${year} </div> <br>  `);
+            });
+            $('.movie-block').click(function () { // grabs the local db ID of the movie clicked
+                console.log('clicked');
+                var tempID = $(this).attr("id");
+                console.log(tempID);
+                deleteConfirm(tempID);
+            });
+        })
+
+
     });
+};
 
-});
+const deleteConfirm = (id) => {
+    $("#dialog").modal({
+        fadeDuration: 100
+    });
+    $("#dialog").html(`<p>Are you sure?</p><a href="#" rel="modal:close"><button>Cancel</button></a><a id="deleteMovieButton" rel="modal:close" style="text-align:right;"><button>DELETE</button></a>`)
+    $('#deleteMovieButton').on('click', function () {
+        deleteMovie(id);
+        loadGallery();
+        $("#admin-options").empty();
+    })
+};
 
+const inlineEdit = () => {
+    $(document).ready(function () {
+        $('.editable').on('click', function () {
+            var that = $(this);
+            console.log(that + " was clicked");
+            if (that.find('input').length > 0) {
+                return;
+            }
+            var currentText = that.text();
+
+            var $input = $('<input>').val(currentText)
+                .css({
+                    'position': 'absolute',
+                    top: '0px',
+                    left: '0px',
+                    width: that.width(),
+                    height: that.height(),
+                    opacity: 0.9,
+                    padding: '10px'
+                });
+
+            $(this).append($input);
+
+            // Handle outside click
+            $(document).click(function (event) {
+                if (!$(event.target).closest('.editable').length) {
+                    if ($input.val()) {
+                        that.text($input.val());
+                    }
+                    that.find('input').remove();
+                }
+            });
+        });
+    })
+};
