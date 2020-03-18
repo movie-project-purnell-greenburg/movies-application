19ef4561e9ed1ffc5a085da4e58ac2aa724b1087
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-03-18 15:27:56 -0500

    finished up our movie project

--- src/index.js
+++ src/index.js
@@ -242,4 +242,17 @@ function serializeToJson(serializer){
     _string += '}';
     return JSON.parse(_string);
 }
+//search bar//
+var myBox = document.getElementById('box');
+myBox.addEventListener('webkitAnimationEnd',function(e) { myBox.style.display = 'none'; }, false);
 
+// updated 2019
+const input = document.getElementById("titleSearch");
+const searchBtn = document.getElementById("search-btn");
+
+const expand = () => {
+    searchBtn.classList.toggle("close");
+    input.classList.toggle("square");
+};
+
+searchBtn.addEventListener("click", expand);
